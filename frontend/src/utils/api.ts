7be00386661e82f9ae2b8892e07f2f8ed380427f
lgomezt <lucasgomeztobon@gmail.com>
2025-11-03 23:12:02 -0500

    Fix env

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,8 +1,12 @@
 // /frontend/src/utils/api.ts
+
+// Get the API base URL from the environment variables
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 export async function fetchProblem(file: File | string) {
   // If file is a string (problem filename), fetch from the backend
   if (typeof file === 'string') {
-    const res = await fetch(`http://localhost:8000/api/problems/${file}`);
+    const res = await fetch(`${API_BASE_URL}/problems/${file}`);
     console.log(res)
     if (!res.ok) {
       throw new Error(`Failed to load problem: ${res.statusText}`);
@@ -15,7 +19,7 @@ export async function fetchProblem(file: File | string) {
   const formData = new FormData();
   formData.append("file", file); // key MUST match 'file' in FastAPI
 
-  const res = await fetch("http://localhost:8000/api/upload", {
+  const res = await fetch("${API_BASE_URL}/upload", {
     method: "POST",
     body: formData,
   });
